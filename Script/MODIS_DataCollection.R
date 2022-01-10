3b18f8e6e2fa090c172ba0251fd5db1ffdfa3282
Anastasios Savvidis <60425699+tasosdsavvidis@users.noreply.github.com>
2022-01-10 13:29:14 +0100

    MODIS image collection

---
+++ Script/MODIS_DataCollection.R
@@ -0,0 +1,23 @@
+# Load packages
+library(rgee)
+library(dplyr)
+library(rgdal)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf); sf::sf_use_s2(FALSE)
+library(tidyverse)
+
+ee_Initialize()
+
+# Load the Study Areas
+ee_roi <- st_read(system.file("pols.shp", package="sf")) %>%
+  st_geometry() %>%
+  sf_as_ee()
+
+
+# Get MODIS Dataset
+surface500m <- ee$ImageCollection("MODIS/006/MOD09GA") %>%
+  ee$ImageCollection$filterDate("2000-02-24", "2021-12-31") %>%
+  ee$ImageCollection$filterBounds(ee_roi)
+
+
