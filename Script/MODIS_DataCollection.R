09fb5a888ec2405f2ad8f336f0bff455a05de703
Anastasios Savvidis <60425699+tasosdsavvidis@users.noreply.github.com>
2022-01-10 17:17:29 +0100

    corrected MODIS filtering

--- Script/MODIS_DataCollection.R
+++ Script/MODIS_DataCollection.R
@@ -6,18 +6,27 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(sf); sf::sf_use_s2(FALSE)
 library(tidyverse)
+library(sp)
 
 ee_Initialize()
 
-# Load the Study Areas
-ee_roi <- st_read(system.file("pols.shp", package="sf")) %>%
-  st_geometry() %>%
-  sf_as_ee()
+# Load the Study Areas and convert them to ee Object
+ee_roi <- st_read("Data/StudyAreas/pols.shp") %>% st_geometry() %>% sf_as_ee()
 
 
-# Get MODIS Dataset
-surface500m <- ee$ImageCollection("MODIS/006/MOD09GA") %>%
-  ee$ImageCollection$filterDate("2000-02-24", "2021-12-31") %>%
-  ee$ImageCollection$filterBounds(ee_roi)
+
+
+# Get MODIS Dataset 
+modis <- ee$ImageCollection("MODIS/006/MOD09GA")
+
+#Filtering
+modis_500m <- ee$ImageCollection("MODIS/006/MOD09GA") %>%
+              ee$ImageCollection$filterDate("2000-02-24", "2021-12-31") %>%
+              ee$ImageCollection$filterBounds(ee_roi) %>%
+              ee$ImageCollection$select("sur_ref1_b01", "sur_ref1_b02", "sur_ref1_b03", "sur_ref1_b04", "sur_ref1_b05", "sur_ref1_b06", "sur_ref1_b07")
+  
+
+
+
 
 
