171327c156e6ebb8735ca31fc306db14c07de044
Anastasios Savvidis <60425699+tasosdsavvidis@users.noreply.github.com>
2022-01-11 09:49:59 +0100

    added quality control for clouds

--- Script/MODIS_DataCollection.R
+++ Script/MODIS_DataCollection.R
@@ -5,7 +5,6 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(sf); sf::sf_use_s2(FALSE)
 library(tidyverse)
-library(sp)
 
 ee_Initialize()
 
@@ -22,12 +21,22 @@ modis <- ee$ImageCollection("MODIS/006/MOD09GA")
 modis_500m <- ee$ImageCollection("MODIS/006/MOD09GA") %>%
               ee$ImageCollection$filterDate("2000-02-24", "2021-12-31") %>%
               ee$ImageCollection$filterBounds(ee_roi) %>%
-              ee$ImageCollection$select("sur_ref1_b01", "sur_ref1_b02", "sur_ref1_b03", "sur_ref1_b04", "sur_ref1_b05", "sur_ref1_b06", "sur_ref1_b07", "QC_500m")
+              ee$ImageCollection$select("sur_refl_b01", "sur_refl_b02","sur_refl_b03", "sur_refl_b04", "sur_refl_b05", "sur_refl_b06", "sur_refl_b07", "QC_500m")
 
 
 
+# Quality Control
 
-
+from <- c(1,3,11,14)
+to   <- c(2,3,11,14)
+reject <- c("01,10", "1", "1", "1")
+qa_bits <- cbind(from, to, reject)
+qa_bits
+##      from to   reject
+## [1,] "1"  "2"  "01,10"  "cloud state"
+## [2,] "3"  "3"  "1"      "cloud shadow"
+## [3,] "11" "11" "1"      "cloud flag"
+## [4,] "14" "14" "1"      "is adjacent to cloud"
 
 
 
