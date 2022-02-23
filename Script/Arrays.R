7c5e3f5346a8dd140a469cd2a666482f5cedc18c
Anastasios Savvidis <60425699+tasosdsavvidis@users.noreply.github.com>
2022-02-23 10:02:08 +0100

    Added ggplot

--- Script/Arrays.R
+++ Script/Arrays.R
@@ -16,7 +16,7 @@ bbox   <- st_bbox(roi %>% st_transform(4326)) %>% st_as_sfc() %>% st_transform(4
 drive         <- "My Drive/Master Thesis/rasters/"
 ## Simeon drive
 # drive         <- "~/Google Drive/My Drive/rasters/"
-flsModis      <- list.files(drive, pattern = "MODIS_ndvi*")   
+flsModis      <- list.files(drive, pattern = "MODIS_ndvi*", recursive = T)   
 flsModis_date <- as.POSIXct(sapply(strsplit(flsModis, "_"), function(x) unlist(strsplit(x[3], ".tif")))) ## that may need to be adjusted depending on your file name. This looked OK as well
 
 ### template of rasters
@@ -57,3 +57,30 @@ mtext("NDSI", 4, line = 3, las = 3)
 par(opar)
 
 ## Not is the best time to get into ggplot and see how you can make a nice graph of this
+
+## To use ggplot we need to convert the array into a dataframe
+
+meltArray <- reshape2::melt(modisArray)
+colnames(meltArray) <- c("Pixel", "DOY", "Index", "Value")
+meltArray$Index <- as.character(meltArray$Index)
+meltArray <- meltArray %>% mutate(Index = recode(Index, '1' = 'NDVI', '2' = 'NDSI'),
+                                  Value = ifelse(Index == 'NDSI', Value / 100, Value))
+
+#Plot
+
+plotArray <- ggplot(meltArray, aes(x = DOY, y = Value, color = Index)) + geom_point() +
+# Custom the Y scales:
+scale_y_continuous(
+  
+  # Features of the first axis
+  name = "NDSI", 
+  
+  # Add a second axis and specify its features
+  sec.axis = sec_axis(trans = ~.* 1 , name = "NDVI")
+)
+
+# Add palette and theme
+library(hrbrthemes)
+plotArray + scale_color_manual(breaks = c("NDSI", "NDVI"),values = c("blue", "green")) + theme_ipsum()
+
+
