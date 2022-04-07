f7a746b38cde9d78e12873d5a512246b69268f7a
Tasos <tasos@Simeons-MBP.dmawi.de>
2022-04-07 14:54:45 +0200

    new script

---
+++ Script/Array data manipulation.R
@@ -0,0 +1,37 @@
+# Load the array and the df
+load("/Users/tasos/Documents/array2001_2012.rda")
+load("/Users/tasos/Documents/array2001_2012df.rda")
+
+
+# Load the rasters and their dates
+drive         <-"/Users/tasos/Documents/20_rasters/"
+flsModis      <- list.files(drive, pattern = "MODIS_ndvi*", recursive = T)   
+flsModis_date <- as.POSIXct(sapply(strsplit(flsModis, "_"), function(x) unlist(strsplit(x[3], ".tif"))))
+
+
+dim(modisArray)
+
+# Calculate the median of NDVI and NDVI for each year
+NDVImedian <- t(apply(modisArray[,,1], 1, function(x) {
+  modisArray[modisArray[,,1] < 0.05] <- NA # Remove the NDVI values below 0.05
+  tapply(x, format(flsModis_date, "%Y"), median, na.rm = T)
+}))
+
+NDSImedian <- t(apply(modisArray[,,2], 1, function(x) {
+  modisArray[,,2] <- modisArray[,,2] /100 #Convert NDSI values to 0 - 1 range
+  tapply(x, format(flsModis_date, "%Y"), median, na.rm = T)
+}))
+
+
+# Make dataframe
+yearMedian <- t(apply(ar[,,1], 1, function(x) {
+  # tapply(x, format(flsModis_date, "%Y"), median, na.rm = T)
+  sapply(split(data.frame(x, joy = as.numeric(format(flsModis_date, "%j"))), format(flsModis_date, "%Y")), function(y) y[min(which(y[,1]<0.1)),2])
+}))
+
+
+
+
+
+
+
