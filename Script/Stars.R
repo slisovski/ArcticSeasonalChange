d8d04a4c1bcd0994895c80482270306b9f972f69
Tasos <tasos@134-1-33-163.awi.de>
2022-06-16 16:07:01 +0200

    update 16.06.2022

--- Script/Stars.R
+++ Script/Stars.R
@@ -18,49 +18,41 @@ flsModis      <- tibble(path = list.files(drive, pattern = "MODIS_ndvi*", recurs
 
 ### template of rasters
 rast_proj <- "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-r0   <- read_stars(paste0(drive, flsModis$path[1])) %>% st_set_crs(rast_proj)
-plot(r0)
 
-grid_bbox <- r0 %>% st_bbox() %>% st_as_sfc() %>% st_make_grid(n = c(2,1))
+r0   <- read_stars(paste0(drive, flsModis$path[1])) %>% st_set_crs(rast_proj)
+plot(r0, axes = T)
+
+
+grid_bbox <- r0 %>% st_bbox() %>% st_as_sfc() %>% st_make_grid(n = c(2,1)) 
 text(grid_bbox %>% st_centroid() %>% st_coordinates(), as.character(1:length(grid_bbox))) 
 
-chunck_01    <- grid_bbox[1]
-chunck_02    <- grid_bbox[2]
-
-crds01 <- r0 %>% as.data.frame() %>% st_as_sf(coords = c("y", "y")) %>%
-  st_set_crs(rast_proj) %>% st_transform(4326) %>% st_coordinates() %>% as_tibble() %>% 
-  rownames_to_column(var = "index") %>% filter(!is.na(r0[])) %>% mutate(index = as.numeric(index))
-
-
-# Create pixel dataframes for each grid cell
-
-
-
-
-# Get the arrays of data for each grid
-
-modisArray01 <- array(dim = c(nrow(crds01), nrow(flsModis), 2)) ## array[pixels, dates, c(ndsi, ndvi)]
-modisArray02 <- array(dim = c(nrow(crds02), nrow(flsModis), 2))
-
-
-
-for(i in 1:nrow(flsModis)) {
+for(i in 1:length(grid_bbox)) {
   
-  ndvi <- raster(paste0(drive, flsModis$path[i]))
-  ndsi <- raster(gsub("MODIS_ndvi", "MODIS_ndsi", paste0(drive, flsModis$path[i])))
+  pol        <- grid_bbox[i]
+  tmp        <- r0 %>% st_set_crs(rast_proj) %>%
+                st_crop(pol %>% st_transform(st_crs(rast_proj)))
   
-  modisArray01[,i,1] <- ifelse(ndvi[][crds01$index] < -1000, NA, ndvi[][crds01$index]) ## at this stage you can remove -9999 to NAs
-  modisArray01[,i,2] <- ifelse(ndsi[][crds01$index] < -1000, NA, ndsi[][crds01$index])
+  modisArray <- array(dim = c(length(c(tmp[[1]])), nrow(flsModis), 2))
+  for(dts in 1:nrow(flsModis)) {
+    
+    ndvi <- read_stars(paste0(drive, flsModis$path[i])) %>% 
+      st_set_crs(rast_proj) %>%
+      st_crop(pol %>% st_transform(st_crs(rast_proj)))
+    
+    ndsi <- read_stars(gsub("MODIS_ndvi", "MODIS_ndsi", paste0(drive, flsModis$path[i]))) %>%
+      st_warp(ndvi)
+    
+    modisArray[,dts,1] <- ifelse(c(ndvi[[1]]) < -1000, NA, c(ndvi[[1]])) ## at this stage you can remove -9999 to NAs
+    modisArray[,dts,2] <- ifelse(c(ndsi[[1]]) < -1000, NA, c(ndsi[[1]]))
+    
+  }
+  
+  chunckOut <- list(rast        = tmp %>% setNames("template"),
+                    date        = flsModis$date,
+                    modisArray  = modisArray)
+  
+  save(chunckOut, file = glue::glue("/Users/tasos/Documents/chunckOut_chnk{i}.rda"))
   
 }
 
-chunckOut01 <- list(rasterIndex = indexRast_01,
-                  date        = flsModis$date,
-                  modisArray  = modisArray01)
 
-chunckOut02 <- list(rasterIndex = indexRast_02,
-                    date        = flsModis$date,
-                    modisArray  = modisArray02)
-
-
-save(chunckOut, file = "C:/Users/tasos/Desktop/Master Thesis Project!/chunkOut.rda")
